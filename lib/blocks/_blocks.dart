9e555d4750b7dc1e5ff11bb35151ec5662e2dcc2
Theus <matheusth.dev@gmail.com>
2023-09-17 20:40:07 -0300

    style: calculator modal

--- lib/blocks/_blocks.dart
+++ lib/blocks/_blocks.dart
@@ -1,1 +1,2 @@
 export './src/form_profile_block.dart' show FormProfile;
+export './src/calculator_modal.dart' show CalculatorModal;
--- lib/pages/src/dashboard_page.dart
+++ lib/pages/src/dashboard_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:imc_calculator/styles/global_styles.dart';
 import 'package:imc_calculator/components/_components.dart';
 import 'package:imc_calculator/models/_models.dart';
+import 'package:imc_calculator/blocks/_blocks.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -61,32 +62,41 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: SafeArea(
         child: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 32),
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Resultados para: ',
-                  style: primaryTextStyle(
-                    size: 30,
-                    weight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  person?.getData()['name'],
-                  style: primaryTextStyle(
-                    size: 30,
-                    weight: FontWeight.bold,
-                  ),
-                ),
-              ],
+            Text(
+              'Resultados para: ${person?.getData()['name']}',
+              textAlign: TextAlign.center,
+              style: primaryTextStyle(
+                size: 30,
+                weight: FontWeight.bold,
+              ),
             ),
             separator(height: 16),
             ...imcResults()
           ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext bc) => Dialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: CalculatorModal(
+                calculate: () {},
+              ),
+            ),
+          );
+        },
+        backgroundColor: primary,
+        child: const Icon(
+          Icons.calculate,
         ),
       ),
     );
