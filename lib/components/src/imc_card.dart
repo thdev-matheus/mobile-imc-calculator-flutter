9b3819a720580bdbdfc45b5fd6230455f556d12c
Theus <matheusth.dev@gmail.com>
2023-09-17 18:29:49 -0300

    feat: imc card component

--- lib/components/src/imc_card.dart
+++ lib/components/src/imc_card.dart
@@ -10,107 +10,141 @@ class ImcCard extends StatelessWidget {
 
   final Imc imc;
 
+  Color getColor(Map<String, dynamic> imcData) {
+    double imcValue = imcData['imc'];
+
+    if (imcValue < 16) {
+      return Colors.red;
+    } else if (imcValue < 17) {
+      return Colors.orange;
+    } else if (imcValue < 18.5) {
+      return Colors.yellow;
+    } else if (imcValue < 25) {
+      return Colors.green;
+    } else if (imcValue < 30) {
+      return Colors.yellow;
+    } else if (imcValue < 35) {
+      return Colors.orange;
+    } else if (imcValue < 40) {
+      return Colors.red;
+    } else {
+      return Colors.purple;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Map<String, dynamic> imcData = imc.getData();
 
     return Card(
+      clipBehavior: Clip.hardEdge,
       elevation: 5,
       child: SizedBox(
         height: 100,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        'Peso: ',
-                        style: primaryTextStyle(
-                          size: 18,
-                          weight: FontWeight.bold,
+        child: Row(
+          children: [
+            Container(
+              width: 10,
+              color: getColor(imcData),
+            ),
+            Expanded(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              'Peso: ',
+                              style: primaryTextStyle(
+                                size: 18,
+                                weight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              '${imcData['weight']}Kg',
+                              style: primaryTextStyle(
+                                size: 18,
+                                weight: FontWeight.bold,
+                                color: primary,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      Text(
-                        '${imcData['weight']}Kg',
-                        style: primaryTextStyle(
-                          size: 18,
-                          weight: FontWeight.bold,
-                          color: primary,
+                        Row(
+                          children: [
+                            Text(
+                              'Altura: ',
+                              style: primaryTextStyle(
+                                size: 18,
+                                weight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              '${imcData['height']}m',
+                              style: primaryTextStyle(
+                                size: 18,
+                                weight: FontWeight.bold,
+                                color: primary,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'Altura: ',
-                        style: primaryTextStyle(
-                          size: 18,
-                          weight: FontWeight.bold,
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "IMC",
+                          style: primaryTextStyle(
+                            size: 18,
+                            weight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      Text(
-                        '${imcData['height']}m',
-                        style: primaryTextStyle(
-                          size: 18,
-                          weight: FontWeight.bold,
-                          color: primary,
+                        Text(
+                          imcData['imc'].toString(),
+                          style: primaryTextStyle(
+                            size: 40,
+                            weight: FontWeight.bold,
+                            color: secondary,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          imcData['day'].toString(),
+                          style: primaryTextStyle(
+                            size: 14,
+                            weight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          imcData['hour'].toString(),
+                          style: primaryTextStyle(
+                            size: 16,
+                            weight: FontWeight.bold,
+                            color: secondary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    "IMC",
-                    style: primaryTextStyle(
-                      size: 18,
-                      weight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    imcData['imc'].toString(),
-                    style: primaryTextStyle(
-                      size: 40,
-                      weight: FontWeight.bold,
-                      color: secondary,
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    imcData['day'].toString(),
-                    style: primaryTextStyle(
-                      size: 14,
-                      weight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    imcData['hour'].toString(),
-                    style: primaryTextStyle(
-                      size: 16,
-                      weight: FontWeight.bold,
-                      color: secondary,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
